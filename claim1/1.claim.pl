35dac3e9e9df36fec194c543a33c777e0c48ab80
Nischal Basuti <nischalbasuti1@gmail.com>
2021-03-28 22:50:01 +0530

    dirty commit nischal

--- claim1/1.claim.pl
+++ claim1/1.claim.pl
@@ -10,3 +10,12 @@ cause_of_damage_to_ship(weapon_of_war, americans).
 contrary(cause_of_damage_to_ship(X,Y), not_cause_of_damage_to_ship(X,Y)).
 
 p1 :- cause_of_damage_to_ship(weapon_of_war, rebels).
+
+d1 :- not_cause_of_damage_to_ship(barratry, crew).
+
+not_cause_of_damage_to_ship(X,Y) :- cause_of_damage_to_ship(A, B), A != X, Y != B.
+argument(a, S).
+
+sub_argument(FUNC, PARAM) :- FUNC(PARAM).
+
+attack(A, B) :- 
